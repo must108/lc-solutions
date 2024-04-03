bcd217bbc4f22084175c39022d18a2cd5c2f14e5
must108 <136145181+must108@users.noreply.github.com>
2024-04-02 23:02:08 -0400

    commit

--- medium/wordsearch.cpp
+++ medium/wordsearch.cpp
@@ -4,14 +4,12 @@ class Solution {
     bool search(vector<vector<char>> &board, string word, int row, int col, int index, int m, int n) {
         if(index == word.length()) {
             return true;
-        }
-
-        if(row < 0 || col < 0 || row == m || col == n || board[row][col] != word[index] || board[row][col] =='.') {
+        } else if(row < 0 || col < 0 || row == m || col == n || board[row][col] != word[index] || board[row][col] == ' ') {
             return false;
         }
 
         char c = board[row][col];
-        board[row][col] = '.';
+        board[row][col] = ' ';
 
         bool up = search(board, word, row - 1, col, index + 1, m, n);
         bool right = search(board, word, row, col + 1, index + 1, m, n);
