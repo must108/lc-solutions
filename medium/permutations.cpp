5823b67c900f9f829e4cea0ea1a521f0cf4ef3a3
must108 <136145181+must108@users.noreply.github.com>
2024-04-04 15:26:07 -0400

    commit

---
+++ medium/permutations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        backtracking(nums, 0, nums.size() - 1, ans);
+        return ans;
+    }
+
+    void backtracking(vector<int>& vect, int l, int r, vector<vector<int>>& retVect) {
+        if(l == r) {
+            retVect.push_back(vect);
+        } else {
+            for(int i = l; i <= r; i++) {
+                swap(vect[l], vect[i]);
+                backtracking(vect, l + 1, r, retVect);
+                swap(vect[i], vect[l]);
+            }
+        }
+    }
+};
