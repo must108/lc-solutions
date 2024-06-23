6eded23ca3f1dd6146ff1fcf3680bd9be32aa3d6
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-23 00:35:55 -0400

    back on the grind

---
+++ python/easy/validanagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        h1 = {}
+        h2 = {}
+
+        for l in s:
+            if l in h1:
+                h1[l] += 1
+            else:
+                h1[l] = 1
+        
+        for l in t:
+            if l in h2:
+                h2[l] += 1
+            else:
+                h2[l] = 1
+
+        return h1 == h2
