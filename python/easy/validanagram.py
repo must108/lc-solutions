2d3868ee64b9097cafe7dc0741971fe36837d906
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-23 16:21:21 -0400

    valid anagram

--- python/easy/validanagram.py
+++ python/easy/validanagram.py
@@ -4,15 +4,9 @@ class Solution:
         h2 = {}
 
         for l in s:
-            if l in h1:
-                h1[l] += 1
-            else:
-                h1[l] = 1
-        
+            h1[l] = 1 + h1.get(l, 0)
+
         for l in t:
-            if l in h2:
-                h2[l] += 1
-            else:
-                h2[l] = 1
+            h2[l] = 1 + h2.get(l, 0)
 
         return h1 == h2
