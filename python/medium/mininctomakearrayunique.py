2b19ce687f92dc10bc1a699f8e7587e6d263dfaa
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-23 21:02:32 -0400

    new

---
+++ python/medium/mininctomakearrayunique.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        count = 0
+
+        for i in range(n-1):
+            if nums[i] >= nums[i+1]:
+                t = nums[i+1]
+                nums[i+1] = nums[i] + 1
+                count += 1 + nums[i] - t
+
+        return count
