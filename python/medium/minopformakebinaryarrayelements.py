842de2865c26f59a7f568d93c84a9870bcef0fc4
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-24 17:33:28 -0400

    problem2

--- python/medium/minopformakebinaryarrayelements.py
+++ python/medium/minopformakebinaryarrayelements.py
@@ -1,14 +1,12 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         count = 0
-        for i in range(len(nums) - 2):
+        n = len(nums)
+        for i in range(n - 2):
             if nums[i] == 0:
-                nums[i] = 1
-                nums[i+1] ^= 1
-                nums[i+2] ^= 1
+                nums[i] = 1-nums[i]
+                nums[i+1] = 1-nums[i+1]
+                nums[i+2] = 1-nums[i+2]
                 count += 1
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                return -1
-        return count
+        return count if sum(nums) == n else -1
